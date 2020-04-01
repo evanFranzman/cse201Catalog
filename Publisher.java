a014d31bdc2b947ec29f8c9ef19caa386b47e65b
CrimsonVanilla <63016596+CrimsonVanilla@users.noreply.github.com>
2020-04-01 15:18:41 -0400

    Create Publisher.java

---
+++ Publisher.java
@@ -0,0 +1,17 @@
+
+public class Publisher {
+
+	// instance variables
+	// name of publisher
+	private String name;
+
+	// default constructor
+	public Publisher() {
+		name = "";
+	}
+
+	// constructor with an input of the desired name of publisher
+	public Publisher(String input) {
+		name = input;
+	}
+}
