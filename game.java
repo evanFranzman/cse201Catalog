5f2903c668cbb83be813dd9aba1926a6d7c54843
KabirArora76 <62124091+KabirArora76@users.noreply.github.com>
2020-04-01 15:09:46 -0400

    Added Game and User Class

---
+++ game.java
@@ -0,0 +1,36 @@
+
+public class game
+{
+
+//Variables
+	int gameId;
+	String name;
+	int publisherId;
+	int genreTypeId;
+	String releaseDate;
+	String currentVersion;
+	int rating;
+	
+//Methods
+	
+	public Array viewComments()
+	{
+		
+	}
+	
+	public Array viewRatings()
+	{
+		
+	}
+	
+	public Array viewReviews()
+	{
+		
+	}
+	
+	public String moreInfo()
+	{
+		
+	}
+
+}
